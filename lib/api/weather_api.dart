6166cd87c7e5180e843d72b2d790a14e8fdd98d7
danifrakanza@gmail.com <danifrakanza@gmail.com>
2025-06-24 20:39:43 +0700

    Menambahkan fungsi getCurrentWeatherByCoordinates berdasarkan lat dan lon

--- lib/api/weather_api.dart
+++ lib/api/weather_api.dart
@@ -6,3 +6,24 @@ import '../utils/app_constants.dart'; // Untuk API_KEY
 class WeatherApi {
   final String _apiKey = AppConstants.openWeatherMapApiKey;
   final String _baseUrl = 'https://api.openweathermap.org/data/2.5';
+
+  Future<Weather> getCurrentWeather(String city) async {
+    final response = await http.get(Uri.parse('$_baseUrl/weather?q=$city&appid=$_apiKey&units=metric'));
+
+    if (response.statusCode == 200) {
+      return Weather.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('Failed to load current weather');
+    }
+  }
+
+  
+  Future<Weather> getCurrentWeatherByCoordinates(double lat, double lon) async {
+    final response = await http.get(Uri.parse('$_baseUrl/weather?lat=$lat&lon=$lon&appid=$_apiKey&units=metric'));
+
+    if (response.statusCode == 200) {
+      return Weather.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('Failed to load current weather by coordinates');
+    }
+  }
